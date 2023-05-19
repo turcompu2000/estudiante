1c74efc705c13e23c2cfd8a82081116f84edcff6
Mauricio Vasquez Villanueva <mauriciovasquezvillanueva@gmail.com>
2023-05-19 17:32:58 -0500

    paquetes vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { createApp } from 'vue';
+
+import App from './App.vue';
+import router from './router';
 
 createApp(App).use(router).mount('#app')
