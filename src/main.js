c036e5fb73a2e3720668eea3fd219b9356ad683b
Mauricio Vasquez Villanueva <mauriciovasquezvillanueva@gmail.com>
2023-05-19 23:47:16 -0500

    paquetes main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,26 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBank,faBuilding, faPencil , faPhone, faPlus, faTag, faTrash, faUser} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { createApp } from 'vue';
 
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app')
+library.add(faPhone);
+library.add(faUser);
+library.add(faTrash);
+library.add(faPencil);
+library.add(faTag);
+library.add(faBuilding);
+library.add(faBank);
+library.add(faBank);
+library.add(faPlus);
+
+
+createApp(App)
+.component("font-awesome-icon",FontAwesomeIcon)
+.use(router)
+.mount('#app')
--- src/views/estudiante.vue
+++ src/views/estudiante.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <h1> LISTADO DE ESTUDIANTES
-            <button @click="newEstudiantes()"
+            <button @click="newEstudiante()"
             class="btn btn-success mx-2">
             <font-awesome-icon icon="plus" />  
             </button>
@@ -14,6 +14,7 @@
                     <th scope="col">Nombre estudiante</th>
                     <th scope="col">Apellido</th>
                     <th scope="col">Carrera</th>
+                    <th scope="col">Actions</th>
                 </tr>
             </thead>
             <tbody>
@@ -23,7 +24,16 @@
                 <td>{{estudiante.Nomb_Estudiante}}</td>
                 <td>{{estudiante.Apellido}}</td>
                 <td>{{estudiante.Carrera}}</td>
-                <td></td>
+                <td>
+                    <button @click="deleteEstudiante(estudiante.id_Estudiante)"
+                      class="btn btn-danger mx-2">
+                      <font-awesome-icon icon="trash" />  
+                    </button>
+                    <button @click="editCategories(estudiante.id_Estudiante)"
+                      class="btn btn-warning mx-2">
+                      <font-awesome-icon icon="pencil" />  
+                    </button>
+                </td>
                 </tr>
             </tbody>
         </table>
@@ -33,12 +43,38 @@
 import axios from 'axios'
 
 export default{
-    name:'Estudiante',
+    name:'Estudiantes',
     data(){
         return{
             estudiante:[]
         }
+    }, 
+    methods:{
+        deleteEstudiante(codigo){
+            Swal.fire({
+                title:`Do you want to delete the comuna with id ${codigo}?`,
+                showCancelButton: true,
+                confirmButtonText: 'Delete',
+            }).then((result) => {
+                    if(result.isConfirmed){
+                        axios.delete(`http://127.0.0.1:8000/api/estudiante/${codigo}`)
+                        .then(response => {
+                            if (response.data.success){
+                                Swal.fire('Deleted!! ', '','success')
+                                this.estudiante=response.data.estudiante
+                            }
+                        })
+                    }
+                })
+        },
+        editEstudiante(id){
+            this.$router.push({name: 'EditarEstudiante', params:{ id: `${id}`}})
+        },
+        newEstudiante(){
+            this.$router.push({name: 'NewEstudiante'});
+        }
     },
+    
     mounted(){
         axios
         .get('http://127.0.0.1:8000/api/estudiante')
